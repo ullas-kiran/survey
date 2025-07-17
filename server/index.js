d9c414a0113577332888dcedc05a1d9ac7832719
ullas-kiran <ullas261@gmail.com>
2025-07-17 16:51:08 +0530

    refactor

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
-const express=require('express');
-const passport=require('passport');
-const GoogleStrategy=require('passport-google-oauth20').Strategy;
-const keys=require('./config/keys')
+const express = require("express");
+require('./services/passport');
 
 
-const app=express();
-passport.use(new GoogleStrategy({
-    clientID:keys.googleClientID,
-    clientSecret:keys.googleClientSecret
-}))
-const PORT=process.env.PORT||5000;
+const app = express();
+require('./routes/authRoutes')(app)
 
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("server running"));
