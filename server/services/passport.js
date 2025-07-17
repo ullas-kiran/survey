d072883e4a686914d423649d70fb677390656b4c
ullas-kiran <ullas261@gmail.com>
2025-07-17 18:38:51 +0530

    passport js fix

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const User=mongoose.model('users')
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -12,9 +12,18 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken,refreshToken,profile,done) => {
-      new User({googleId:profile.id}).save()
-      console.log(accessToken,refreshToken,profile,done);
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // already have record
+          done(null,existingUser)
+        } else {
+          // we dont have the user
+          new User({ googleId: profile.id }).save();
+        }
+      });
+
+      console.log(accessToken, refreshToken, profile, done);
     }
   )
 );
