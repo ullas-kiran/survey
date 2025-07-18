e9493da7b135f09e0d2068ea40ffc47321102715
ullas-kiran <ullas261@gmail.com>
2025-07-18 19:14:53 +0530

    feat(passport):cookies

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,8 +2,19 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 const mongoose = require("mongoose");
+const cookiesSession=require('cookie-session');
+const passport=require('passport');
 
-const User = mongoose.model("users");
+  const User = mongoose.model("users");
+  passport.serializeUser((user,done)=>{
+  done(null,user.id)
+  })
+
+  passport.deserializeUser((id,done)=>{
+    User.findById(id).then(user=>{
+      done(null,user)
+    })
+  })
 
 passport.use(
   new GoogleStrategy(
@@ -19,7 +30,7 @@ passport.use(
           done(null,existingUser)
         } else {
           // we dont have the user
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id }).save().then(user=>done(null,user));
         }
       });
 
